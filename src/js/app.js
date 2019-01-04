c0f21387901c5614ab328ab97caf441e78dc86b1
Tanatwat Wattanasakul <scomusicrusted@gmail.com>
2019-01-04 14:39:54 +0700

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import sails from './dependencies/sails.io.js'
+
+window._ = require('lodash');
+window.$ = window.jQuery = require('jquery');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.Vue = require('vue');
